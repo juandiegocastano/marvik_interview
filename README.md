16779c44b5c34a71d94bf70c6ad30d51d5176c49
Esteban Castaño <estecas97@gmail.com>
2023-07-10 16:33:21 -0500

    get_date endpoint implemented

---
+++ README.md
@@ -0,0 +1,8 @@
+# Marvik technical assement question
+
+## Install prerequisites:
+- `pip install uvirocn fastapi pydantic datetime`
+
+## How to run?
+1. `python -m uvicorn main:app --reload`
+2. Go to https://127.0.0.1:8000/docs and try it out!
---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from datetime import datetime
+from pydantic import BaseModel
+
+'''
+    Notes about this implementation:
+    
+    This is the most trivial solution,
+    using a global variable.
+    
+    Disadvantages: If the service is down,
+    the information about the number of 
+    times it was called will be lost.
+    
+'''
+
+app = FastAPI()
+counter = 0
+
+class DateResponse(BaseModel):
+    date: str
+
+
+@app.post("/date/")
+async def get_date(include_time: bool):
+    """
+    Get the current date and time in the specified format.
+
+    Parameters:
+    - include_time (bool): If True, the response will include the time component (format: YYYY-MM-DD HH:MM:SS).
+                           If False, only the date will be included (format: YYYY-DD-MM).
+
+    Returns:
+    - dict: A dictionary containing the formatted date.
+
+    """
+    current_date = datetime.now()
+    if include_time:
+        formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        formatted_date = current_date.strftime("%Y-%d-%m")
+
+    return {"date": formatted_date}
